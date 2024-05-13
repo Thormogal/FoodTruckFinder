a50a603dc3a46332a8afd131f4a77aacfff80720
Oskar Lövstrand <Oskar.lovstrand@hotmail.com>
2024-05-13 09:24:26 +0200

    First version

---
+++ FoodTruckFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodTruckFinder
+//
+//  Created by Oskar Lövstrand on 2024-05-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodTruckFinder/FoodTruckFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTruckFinderApp.swift
+//  FoodTruckFinder
+//
+//  Created by Oskar Lövstrand on 2024-05-13.
+//
+
+import SwiftUI
+
+@main
+struct FoodTruckFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
