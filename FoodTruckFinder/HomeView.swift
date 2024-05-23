6b4cb3da148c1f92acbcb1531aa5f703f209f8aa
alvinjhson <alvinbest26@gmail.com>
2024-05-23 14:44:58 +0200

    select category works now

--- FoodTruckFinder/HomeView.swift
+++ FoodTruckFinder/HomeView.swift
@@ -23,52 +23,103 @@ extension UIColor {
         return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
     }
 }
+import SwiftUI
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+struct Category:Equatable, Identifiable {
+    var id = UUID()
+    var name: String
+    var imageName: String
+}
+
+struct CategoryItemView: View {
+    var category: Category
+    var isSelected: Bool
+    
+    var body: some View {
+        VStack {
+            Image(category.imageName)
+                .resizable()
+                .frame(width: 50, height: 50)
+                .clipShape(Circle())
+                .overlay(
+                    Circle().stroke(isSelected ? Color.blue : Color.clear, lineWidth: 2)
+                )
+            Text(category.name)
+                .font(.caption)
+                .foregroundColor(isSelected ? .blue : .primary)
+        }
+    }
+}
+
 
 struct HomeView: View {
     @State private var foodTrucks: [FoodTruck] = []
+    @State private var filteredFoodTrucks: [FoodTruck] = []
+    @State private var selectedCategory: Category? = nil
+    @State private var categories: [Category] = [
+        Category(name: "All", imageName: "all"),
+        Category(name: "Taco", imageName: "taco"),
+        Category(name: "Sushi", imageName: "sushi"),
+        Category(name: "Hamburger", imageName: "hamburger"),
+        Category(name: "Asian", imageName: "asian"),
+        Category(name: "Indian", imageName: "indian"),
+        Category(name: "Pizza", imageName: "pizza"),
+        Category(name: "Kebab", imageName: "kebab"),
+        Category(name: "Chicken", imageName: "chicken")
+    ]
     
     var body: some View {
         NavigationStack {
-            List(foodTrucks) { foodTruck in
-                VStack(alignment: .leading) {
-                    Text(foodTruck.name)
-                        .font(.headline)
-                        .padding(.bottom, 5)
-                        .frame(maxWidth: .infinity)
-                        .multilineTextAlignment(.center)
-                    
+            VStack {
+          
+                ScrollView(.horizontal, showsIndicators: false) {
                     HStack {
-//                        if let imageURL = URL(string: foodTruck.imageURL) {
-//                            AsyncImage(url: imageURL) { phase in
-//                                if let image = phase.image {
-//                                    image.resizable()
-//                                         .frame(width: 100, height: 100)
-//                                         .clipShape(Circle())
-//                                } else {
-                                    Image("cardpic")
-                                        .resizable()
-                                        .frame(width: 100, height: 100)
-                                        .clipShape(Circle())
-//                                }
-//                            }
-//                        }
-                        Spacer()
-                        VStack(alignment: .leading) {
-                            Text(foodTruck.openingHours)
-                            Text("Food: \(foodTruck.foodType)")
-                            RtingView(rating: Int(foodTruck.rating)) // Pass rating directly
+                        ForEach(categories) { category in
+                            CategoryItemView(category: category, isSelected: category == selectedCategory)
+                                .padding(.horizontal, 10)
+                                .onTapGesture {
+                                    selectedCategory = category
+                                    filterFoodTrucks()
+                                }
                         }
-                        Spacer()
                     }
+                    .padding()
                 }
-                .padding()
-                .background(Color(UIColor.fromHex("3D84B7")))
-                .foregroundColor(Color.white)
-                .cornerRadius(10)
-                .padding(.vertical, 5)
-            }
-            .onAppear {
-                fetchFoodTrucks()
+                
+                
+                List(filteredFoodTrucks) { foodTruck in
+                    VStack(alignment: .leading) {
+                        Text(foodTruck.name)
+                            .font(.headline)
+                            .padding(.bottom, 5)
+                            .frame(maxWidth: .infinity)
+                            .multilineTextAlignment(.center)
+                        
+                        HStack {
+                            Image("cardpic")
+                                .resizable()
+                                .frame(width: 100, height: 100)
+                                .clipShape(Circle())
+                            Spacer()
+                            VStack(alignment: .leading) {
+                                Text(foodTruck.openingHours)
+                                Text("Food: \(foodTruck.foodType)")
+                                RtingView(rating: Int(foodTruck.rating)) 
+                            }
+                            Spacer()
+                        }
+                    }
+                    .padding()
+                    .background(Color(UIColor.fromHex("3D84B7")))
+                    .foregroundColor(Color.white)
+                    .cornerRadius(10)
+                    .padding(.vertical, 5)
+                }
+                .onAppear {
+                    fetchFoodTrucks()
+                }
             }
         }
     }
@@ -87,11 +138,26 @@ struct HomeView: View {
             }
             
             self.foodTrucks = documents.compactMap { document -> FoodTruck? in
-                try? document.data(as: FoodTruck.self)
+                do {
+                    let foodTruck = try document.data(as: FoodTruck.self)
+                    return foodTruck
+                } catch {
+                    print("Error decoding document into FoodTruck: \(error.localizedDescription)")
+                    return nil
+                }
+            }
+            filterFoodTrucks()
+        }
+    }
+    
+    private func filterFoodTrucks() {
+            if let selectedCategory = selectedCategory, selectedCategory.name != "All" {
+                filteredFoodTrucks = foodTrucks.filter { $0.foodType == selectedCategory.name }
+            } else {
+                filteredFoodTrucks = foodTrucks
             }
         }
     }
-}
 
 
 
